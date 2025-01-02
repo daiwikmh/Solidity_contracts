50f24fbd041d72a777e1a8d5711a97d982532f99
daiwikmh <daiwikdomain@gmail.com>
2025-01-02 23:31:18 +0530

    employee

--- contracts/3/childOrgEnrollment.sol
+++ contracts/3/childOrgEnrollment.sol
@@ -1,24 +1,35 @@
+
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-contract Parentorg{
+contract ParentOrg {
     struct EmployeeDetails {
         string name;
         uint age;
         string department;
     }
 
-    mapping(address =>EmployeeDetails ) details;
+    mapping(address => EmployeeDetails) public details;
 
-    function enroll(string memory _name, uint _age, string memory _department)virtual external{
+    function enroll(string memory _name, uint _age, string memory _department) virtual external {
         details[msg.sender] = EmployeeDetails(_name, _age, _department);
     }
 }
 
-contract newcompany is Parentorg{
-        function enroll(string memory _name, uint _age, string memory _department)override public{
+contract NewCompany is ParentOrg {
+    function enroll(string memory _name, uint _age, string memory _department) virtual override public {
+                        details[msg.sender] = EmployeeDetails(_name, _age, _department );
 
-                details[msg.sender] = EmployeeDetails(_name, _age, _department );
-
+    }
 }
+
+contract AnotherCompany is ParentOrg {
+    function enroll(string memory _name, uint _age, string memory _department) virtual override public {
+                        details[msg.sender] = EmployeeDetails(_name, _age, _department );
+
+    }
+
+    function getDepartment(address _employee) public view returns (string memory) {
+        return details[_employee].department;
+    }
 }
